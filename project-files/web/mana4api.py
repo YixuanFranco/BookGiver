86255951a834aa3f6c3370e8622031cedf965dc4
chao <chao@ubuntu.com>
2015-05-27 22:58:53 +0800

    KVDB for book info

--- project-files/web/mana4api.py
+++ project-files/web/mana4api.py
@@ -13,6 +13,20 @@ import xml.etree.ElementTree as ET
 APP = Bottle()
 import sae.kvdb
 KV = sae.kvdb.KVClient()
+#key = 书名+openid
+#value = 南京.5月26日.6月1日
+
+bk = sae.kvdb.KVClient()
+#key = book+time()
+#value = 书名
+
+
+b = "借书：b_书名(创造力)_城市.开始时间(x月y日).结束时间(x月y日)"
+a = "查询被提交的书目：a"
+m = "查看某个书目的所有借阅请求：m_书名(创造力)"
+s = "查看自己提交的所有请求：s"
+d = "删除请求：d_书名(创造力)"
+
 
 @APP.get('/')
 def web_access():
@@ -41,48 +55,110 @@ def wechat_post():
      __MsgType = xml.findtext("MsgType")
      __Content = xml.findtext("Content")
      req = []
-     key = []
+
+     index = []
+     key = ""
+     value = ""
+     bk.index = []
+     bk.key = ""
+     bk.value = ""
+ #    content = ""
+     book = ""
+     con = ""
+
      if "text" == __MsgType:
         req = __Content.split('_')
-        if "hi" == req[0]:
-            tStamp = time.time()
+        if "hi" == req[0] or "HI" == req[0] or "Hi" == req[0]:
             content = "欢迎！输入 h 继续"
-        elif "h" == req[0]:
-            tStamp = time.time()
-            content = "借书：b_书名(创造力)_城市.开始时间(x月y日).结束时间(x月y日)；查询：a_书名(创造力)"
+
+        elif "h" == req[0] or "H" == req[0]:
+            content = "%s \n%s \n%s \n%s \n%s"% (b,a,m,s,d)
             #b+创造力+南京.5月26日.6月1日
             #a+创造力
-        elif "a" == req[0]:
+
+        elif "a" == req[0] or "A" == req[0] :
+            bk_index = bk.getkeys_by_prefix("book", limit=100, marker=None)
+            for i in bk_index:
+               con = con + "\n %s"% bk.get(i)
+            content = "all books %s"% con
+
+        elif "m" == req[0] or "M" == req[0] :
             book = str(req[1].encode('UTF-8'))
-            tStamp = time.time()
             index = KV.getkeys_by_prefix(book, limit=100, marker=None)
-            print index
-            #['\xe5\x88\x9b\xe9\x80\xa0\xe5\x8a\x9b1432571672.73']
-            con = ""
+            key_spl = []
             for i in index:
-               con = con + "%s \n"% KV.get(i)
-               print KV.get(i)
+               key_spl = i.split('+')
+               con += "\n %s.%s"% (key_spl[1] ,KV.get(i) ) 
+
                #ovcFVtwLC-7dEArda7gzgfgOPaH0.南京.5月26日.6月1日
-            print con
-            content = "%s \n %s"% (book.decode('UTF-8'),con)
+
+            content = "%s %s"% (book.decode('UTF-8'),con)
             #创造力 
             #ovcFVtwLC-7dEArda7gzgfgOPaH0.南京.5月26日.6月1日 
             #ovcFVtwLC-7dEArda7gzgfgOPaH0.南京.5月26日.6月1日
-        elif "b" == req[0]:
-            key = str(req[1].encode('UTF-8')) + str(time.time())
-            print key
-            #创造力1432571303.12
-            value = toUser + "." + req[2]
-            print value
-            #ovcFVtwLC-7dEArda7gzgfgOPaH0.南京.5月26日.6月1日
+ 
+        elif "b" == req[0] or "B" == req[0]:
+            book = str(req[1].encode('UTF-8'))
+            #创造力
+
+            key = book + "+" + toUser
+            #创造力+ovcFVtwLC-7dEArda7gzgfgOPaH0
+            value = req[2]
+            #南京.5月26日.6月1日
             KV.add(key, value)
-            tStamp = time.time()
-            content = "%s OK :)"% (req[1] + "." + req[2]) 
+
+            content = "%s OK :)"% (book.decode('UTF-8') + "." + req[2]) 
             #创造力.南京.5月26日.6月1日
+
+
+            #保存书目的数据库，用于查询所有需要借阅的书籍信息
+            bk_key = "book" + str(time.time())
+            bk_value = str(req[1].encode('UTF-8'))
+            true = 0
+            bk_index = bk.getkeys_by_prefix("book", limit=100, marker=None)
+            for i in bk_index:
+               if bk.get(i) == bk_value :
+                  true = 1
+            if true == 0:
+               bk.add(bk_key , bk_value)
+
+        elif "s" == req[0] or "S" == req[0]:
+            bk_index = bk.getkeys_by_prefix("book", limit=100, marker=None)
+
+            for i in bk_index:
+               key = bk.get(i) + "+" + toUser
+               if KV.get(key) != None :
+                  con = con + ("%s.%s \n"% ((bk.get(i)).decode('UTF-8'),KV.get(key)))
+
+            content = "%s "% con
+        
+        elif "d" == req[0] or "D" == req[0]:
+            book = str(req[1].encode('UTF-8'))
+            key = book + "+" + toUser
+            print key
+            KV.delete(key)
+
+            index = KV.getkeys_by_prefix(book, limit=100, marker=None)
+            count = 0
+            for i in index :
+               count += 1
+            if count == 0 :  #查找是否还有本书的借阅请求，如果没有
+               bk_index = bk.getkeys_by_prefix("book", limit=100, marker=None)
+               print bk_index
+               for j in bk_index:
+                  print j
+                  print bk.get(j)
+                  if bk.get(j) == book :                                   #查找书目里是否还有该书，如果有
+                    bk.delete(j) 
+
+            content = "%s deleted"% book.decode('UTF-8')
+
         else :
-            tStamp = time.time()
             content = "输入 h 继续使用"
+
+        tStamp = time.time()
         print CFG.TPL_TEXT% locals()
         return CFG.TPL_TEXT% locals()
      return None
+
 
